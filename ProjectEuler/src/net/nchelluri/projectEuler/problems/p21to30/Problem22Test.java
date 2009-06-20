057b6943e52c1a6578649525ff78cd3e6ef09366
nchelluri <nchelluri@a14c68a0-3788-11de-a9fe-ad1e22fc3a8a>
2009-06-20 18:16:28 +0000

    [Added] p.22;

---
+++ ProjectEuler/src/net/nchelluri/projectEuler/problems/p21to30/Problem22Test.java
@@ -0,0 +1,9 @@
+package net.nchelluri.projectEuler.problems.p21to30;
+
+import junit.framework.TestCase;
+
+public class Problem22Test extends TestCase {
+	public void testAlphavalue() {
+		assertEquals(53, Problem22.alphavalue("COLIN"));
+	}
+}
