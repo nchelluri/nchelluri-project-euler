9079692f928a5dfe06cce29096eec2bb63af2913
nchelluri <nchelluri@a14c68a0-3788-11de-a9fe-ad1e22fc3a8a>
2009-11-08 11:57:20 +0000

    [ADDED] Memory hog solution to p.24!!! I'm back, baby... Level 1 bitches

---
+++ trunk/ProjectEuler/src/net/nchelluri/projectEuler/problems/p21to30/Problem24.java
@@ -0,0 +1,72 @@
+package net.nchelluri.projectEuler.problems.p21to30;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Problem24 {
+	public static void main(String[] args) {
+		long time = System.currentTimeMillis();
+
+		List<Integer> pool = new ArrayList<Integer>();
+		pool.add(0);
+		pool.add(1);
+		pool.add(2);
+		pool.add(3);
+		pool.add(4);
+		pool.add(5);
+		pool.add(6);
+		pool.add(7);
+		pool.add(8);
+		pool.add(9);
+		List<List<Integer>> permutations = permute(pool);
+		System.out.println(permutations.get(999999));
+
+		System.out.println("Execution time: "
+				+ (System.currentTimeMillis() - time) + "ms");
+	}
+
+	protected static List<List<Integer>> permute(List<Integer> pool) {
+		List<List<Integer>> retVal = new ArrayList<List<Integer>>();
+
+		if (pool.size() == 2) {
+			int first = pool.get(0);
+			int second = pool.get(1);
+
+			if (first < second) {
+				List<Integer> firstPerm = new ArrayList<Integer>();
+				firstPerm.add(first);
+				firstPerm.add(second);
+				retVal.add(firstPerm);
+
+				List<Integer> secondPerm = new ArrayList<Integer>();
+				secondPerm.add(second);
+				secondPerm.add(first);
+				retVal.add(secondPerm);
+			} else {
+				List<Integer> firstPerm = new ArrayList<Integer>();
+				firstPerm.add(second);
+				firstPerm.add(first);
+				retVal.add(firstPerm);
+
+				List<Integer> secondPerm = new ArrayList<Integer>();
+				secondPerm.add(first);
+				secondPerm.add(second);
+				retVal.add(secondPerm);
+			}
+		} else {
+			Collections.sort(pool);
+			for (int i = 0; i < pool.size(); i++) {
+				List<Integer> subList = new ArrayList<Integer>(pool);
+				subList.remove(i);
+				List<List<Integer>> subPermutations = permute(subList);
+				for (List<Integer> subPermutation : subPermutations) {
+					subPermutation.add(0, pool.get(i));
+					retVal.add(subPermutation);
+				}
+			}
+		}
+
+		return retVal;
+	}
+}
